f265c1b7599c409e557158ab24896be3d50e8dda
Reen00ad <105815655+Reen00ad@users.noreply.github.com>
2024-02-12 20:59:44 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,171 @@
+import java.util.Scanner;
+import java.util.SortedMap;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        Scanner s = new Scanner(System.in);
+        // ex 1
+//       System.out.println("weight(KG) = " );
+//        double weight = s.nextDouble() ;
+//       System.out.println("height(m) = " );
+//        double height = s.nextDouble();
+//        double BMI =weight / (height * height);
+//        System.out.println("BMI = " + BMI);
+
+        // ex2
+//       System.out.println ("obtained marks = ");
+//        double obtaine = s.nextDouble();
+//        System.out.println("total mark = ");
+//        double total = s.nextDouble();
+//        double percentage = ((obtaine / total) * 100 );
+//        System.out.println("percentage = " + percentage + "%");
+
+        // ex3
+//       System.out.print("amount in USD = ");
+//        double amountusd = s.nextDouble();
+//        System.out.print("exchange rate (USD TO EUR) = ");
+//        double exchangerate = s.nextDouble();
+//        double amounteur = amountusd * exchangerate;
+//        System.out.print("amount in EUR = " + amounteur);
+
+        // ex4
+//        System.out.print("enter the world ");
+//        String word = s.nextLine();
+//       System.out.println("length of the string " + word.length());
+//       String reverse = "";
+//       for(int i = word.length()-1; i>=0; i--){
+//           reverse = reverse+word.charAt(i);
+//       }
+//
+//        System.out.println("reversed string : "+reverse);
+
+        //ex5
+//        System.out.print("sentence =" );
+//        String sentence = s.nextLine() ;
+//        String a = sentence.substring(10 , 20);
+//        System.out.print(a);
+
+        //ex6
+//        System.out.print("sentence = ");
+//        String sen = s.nextLine();
+//        System.out.print("keyword = ");
+//        String keyword = s.nextLine();
+//        if (sen.contains(keyword)) {
+//
+//            System.out.print("keyword " + keyword + " is present in the sentence ");
+//        }
+        //ex7
+//        System.out.print("sentence = ");
+//        String sen = s.nextLine();
+//        System.out.print("word to replace = ");
+//        String replace = s.nextLine();
+//        System.out.print("replacement word = ");
+//        String rep = s.nextLine();
+//        System.out.println("the new sentence  " + sen.replace(replace ,rep));
+        //ex8
+//        System.out.print("String 1 = ");
+//        String s1 = s.nextLine();
+//        System.out.print("String 2 = ");
+//        String s2 = s.nextLine();
+//        if(s1.equalsIgnoreCase(s2))
+//        System.out.print("strings are equal");
+
+        //ex 2
+        //ex1
+//        System.out.print("enter your role : ");
+//        String role = s.nextLine();
+//        if (role.equalsIgnoreCase("admin"))
+//            System.out.print("welcome admin");
+//        else if (role.equalsIgnoreCase("superuser"))
+//            System.out.print("welcome superuser");
+//        else if (role.equalsIgnoreCase("user"))
+//            System.out.print("welcome user");
+//        else
+//            System.out.print("try again");
+
+        //ex2
+//        System.out.print("input the 1st number : ");
+//        int num1 = s.nextInt();
+//        System.out.print("input the 2nd number : ");
+//        int num2 = s.nextInt();
+//        System.out.print("input the 3rd number : ");
+//        int num3 = s.nextInt();
+//        if(num1>=num2 && num1>=num3)
+//            System.out.print( "the greatest "+num1);
+//        else if(num2>=num1 && num2>=num3)
+//            System.out.print( "the greatest "+num2);
+//        else System.out.print("the greatest "+num3);
+
+        //ex3
+//        System.out.print("enter the number ");
+//        int num = s.nextInt();
+//        switch (num){
+//            case 1 :
+//                System.out.print("Sunday");
+//                break;
+//            case 2 :
+//                System.out.print("Monday");
+//                break;
+//            case 3 :
+//                System.out.print("Tuesday");
+//                break;
+//            case 4 :
+//                System.out.print("Wednesday");
+//                break;
+//            case 5 :
+//                System.out.print("Thursday");
+//                break;
+//            case 6 :
+//                System.out.print("Friday");
+//                break;
+//            case 7 :
+//                System.out.print("Saturday");
+//                break;
+//            default :
+//                System.out.print("The number must be from 1 to 7 ");
+        //ex4
+//        System.out.print("Enter your numeric score : ");
+//        int num=s.nextInt();
+//        System.out.println("Numeric score "+num);
+//        if (num>=90 && num<=100)
+//            System.out.print("Leeter grade : A");
+//        else if (num>=80 && num<=89)
+//            System.out.print("Leeter grade : B");
+//        else if (num>=70 && num<=79)
+//            System.out.print("Leeter grade : C");
+//        else if (num>=60 && num<=69)
+//            System.out.print("Leeter grade : D");
+//        else if (num>=0 && num<=59)
+//            System.out.print("Leeter grade : F");
+//        else
+//            System.out.print("the grade must be from 0 to 100 ");
+        //ex5
+//        System.out.print("Enter your age : ");
+//        int age = s.nextInt();
+//        if (age>=0 && age< 13)
+//            System.out.print("you ar an Childe");
+//        else if (age>13 && age<=19)
+//            System.out.print("you are an Tennager");
+//        else if (age>=20)
+//            System.out.print("you are an Adult");
+//        else
+//            System.out.print("the age must be greater than 0 ");
+
+
+
+
+        }
+
+
+    }
+
+
+
+
+
+
+
